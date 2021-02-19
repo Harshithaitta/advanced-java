d3a8a581643f458543484c49a9dd391996138ed7
digital9999 <78781058+digital9999@users.noreply.github.com>
2021-02-19 14:43:09 +0530

    Update lockapp.java

--- lockapp.java
+++ lockapp.java
@@ -1,4 +1,7 @@
-lockapp.java
+Create a Java Swing GUI application for an electronic lock as shown below. The display shall show the state of either "CLOSE" or "OPEN". In
+the ";CLOSE" state, the user types his PIN followed by the "Enter" key to unlock the system. The display shall show an asterisk (*) for each
+number entered. The display shall show "WRONG PIN" if the PIN is incorrect. The "Clear" button clears the number entered (if any), locks the
+system and sets the display to "CLOSE".
 
 import javax.swing.*;
 import java.awt.*;
